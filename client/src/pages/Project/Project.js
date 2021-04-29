a0773c2af2eb96bcf126eb297541f12771760111
Matt Autrey <mautrey17@gmail.com>
2021-04-28 21:18:00 -0400

    project page with kanban columns

--- client/src/pages/Project/Project.js
+++ client/src/pages/Project/Project.js
@@ -1,17 +1,33 @@
 import React from "react";
+import {Col, Row} from "../../components/Grid";
+import KanBan from "../../components/KanBan"
 
 function Project() {
 
     return(
-        <div>
+        <div className="container">
             <div>
-                <h1>Component to hold project creation</h1>
+                <h1>Graph of Task Statuses</h1>
             </div>
             <div>
                 <h1>Important user info and due dates</h1>
             </div>
             <div>
-                <h1>Active Projects</h1>
+            <h1>Active Projects</h1>
+                <Row>
+                    
+                        <KanBan 
+                            title="To Do"
+                        />
+                        <KanBan 
+                            title="In Progress"
+                        />
+                        <KanBan 
+                            title="Completed"
+                        />
+                        
+                </Row>
+                
             </div>
         </div>
     )
