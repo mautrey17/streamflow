348745c2a8df89f953dcbc5dae514def51eabf22
unknown <nhounhou@hotmail.com>
2021-05-01 13:26:21 -0400

    Navbar on all pages

--- client/src/pages/Messages/Messages.js
+++ client/src/pages/Messages/Messages.js
@@ -1,46 +1,19 @@
 import React from 'react';
-import LeftContainer from '../../components/LeftContainer/LeftContainer';
-import {Col, Row} from "../../components/Grid";
-import ActiveMessage from '../../components/ActiveMessage';
+// import LeftContainer from '../../components/LeftContainer/LeftContainer';
+// import {Col, Row} from "../../components/Grid";
+// import ActiveMessage from '../../components/ActiveMessage';
+import Nav from "../../components/Nav";
 
 function Messages() {
-
-    // return(
-    //     <div>
-    //         <h1>Chat App</h1>
-    //         <Row>
-    //             <LeftContainer />
-    //             <Col size="md-1 sm-1" />
-    //             <Col size="md-7 sm-7">
-    //             <div className="chat" >
-    //                 <iframe
-    //                     title='Chat App Frame'
-    //                     src='https://chat-app-unc.netlify.app/'
-    //                     width='100%'
-    //                     height='600'></iframe>
-    //                 <h1 className="text-center mt-3 test">Your Conversation with Fake Person</h1>
-    //                 <ActiveMessage 
-    //                     sent={'to'}
-    //                     message={'Hey this is a super important message'}
-    //                 />
-    //                 <ActiveMessage 
-    //                     sent={'from'}
-    //                     message={'Here is my reply'}
-    //                 />
-    //             </div>
-                
-    //             </Col>
-    //         </Row>
-    //     </div>
-    // )
     console.log("messaging");
     return (
         <div>
+            <Nav />
             <iframe
                 title='Chat App Frame'
                 src='https://chat-app-unc.netlify.app/'
                 width='100%'
-                height='600'>
+                height='800'>
             </iframe>
         </div>
     )
