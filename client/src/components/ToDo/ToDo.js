5f36962a611b0726e525387976d78eecc7438cb4
LinosM <Zemnas768@gmail.com>
2021-05-04 01:16:53 -0400

    rendering tasks to dashboard (WIP)

--- client/src/components/ToDo/ToDo.js
+++ client/src/components/ToDo/ToDo.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API";
 
 function ToDo() {
+    const [assignedTasks, setAssignedTasks] = useState([]);
+    const [currentDate, setCurrentDate] = useState(new Date());
 
+    useEffect(() => {
+        loadAssignedTasks();
+    }, []);
+
+    function loadAssignedTasks() {
+        API.getTasks("assigned")
+            .then(res => {
+                setAssignedTasks(res.data.tasks);
+            })
+    }
+
+    function compareTime(x) {
+        const dateB = new Date(x);
+        console.log("current date: " + currentDate.getDate())
+        console.log("dateB: " + dateB.getDate())
+        if (currentDate.getMonth() === dateB.getMonth()) {
+            if ( parseFloat(currentDate.getDate()) < ( parseFloat(dateB.getDate()) )) {
+                return 3
+            }
+        }
+    }
 
     return(
         <div>
@@ -88,24 +112,21 @@ function ToDo() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Project 1</td>
-                        <td>Finish Home Page</td>
-                        <td>Urgent</td>
-                        <td>Stuck</td>
-                        <td>90%</td>
-                        <td>Jill</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Project 1</td>
-                        <td>Finish Home Page</td>
-                        <td>Moderate</td>
-                        <td>Flowing</td>
-                        <td>60%</td>
-                        <td>Jack</td>
-                    </tr>
+                    {assignedTasks.map((task, i) => (
+                        <>
+                            {compareTime(task.dueDate) === 3 && 
+                                <tr>
+                                    <th scope="row">{i+1}</th>
+                                    <td>Project 1</td>
+                                    <td>{task.title}</td>
+                                    <td>{task.urgency.charAt(0).toUpperCase() + task.urgency.slice(1)}</td>
+                                    <td>{task.status.charAt(0).toUpperCase() + task.urgency.slice(1)}</td>
+                                    <td>90%</td>
+                                    <td>{task.owner.firstName}</td>
+                                </tr>
+                            }
+                        </>
+                    ))}
                 </tbody>
             </table>
         </div>
