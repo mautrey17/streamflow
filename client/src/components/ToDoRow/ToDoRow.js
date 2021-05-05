0f637f90f835f87d0c1faec2b98349d0be4e6b21
LinosM <Zemnas768@gmail.com>
2021-05-05 16:52:01 -0400

    Status is correctly displayed now

--- client/src/components/ToDoRow/ToDoRow.js
+++ client/src/components/ToDoRow/ToDoRow.js
@@ -2,14 +2,31 @@ import React, { useState, useEffect } from "react";
 
 function ToDoRow(props) {
 
+    function status(x) {
+        switch(x) {
+            case "onTrack":
+                return "On Track";
+            case "potentialDelays":
+                return "Potential Delays";
+            case "delayed":
+                return "Delayed";
+            case "stuck":
+                return "Stuck";
+            case "finished":
+                return "Finished";
+            default:
+                return "";
+        }
+    }
+
     return (
         <tr>
             <th scope="row">{props.count}</th>
-            <td>Project 1</td>
+            <td>{props.project}</td>
             <td>{props.task.title}</td>
             <td>{props.task.urgency ? props.task.urgency.charAt(0).toUpperCase() + props.task.urgency.slice(1) : ""}</td>
-            <td>{props.task.status ? props.task.status.charAt(0).toUpperCase() + props.task.urgency.slice(1) : ""}</td>
-            <td>90%</td>
+            <td>{props.task.status ? status(props.task.status) : ""}</td>
+            <td></td>
             <td>{props.task.owner.firstName}</td>
         </tr>
     )
