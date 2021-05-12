216e3170a109324ffa6da01e7d3f523bd76dec1e
LinosM <Zemnas768@gmail.com>
2021-05-12 19:09:26 -0400

    fixed duplicate react and errors

--- server/routes/api/avatar.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const avatarController = require("../../controllers/avatarController");
-
-//Match api/avatar
-
-router.route("/:id")
-.update(avatarController.update);
-
-module.exports = router;
