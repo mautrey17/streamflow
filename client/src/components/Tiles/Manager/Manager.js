1a61a09f49cdba2c4ef21bbced1c933a922c9131
LinosM <Zemnas768@gmail.com>
2021-05-18 00:25:00 -0400

    manager should no longer error if avatar not loaded

--- client/src/components/Tiles/Manager/Manager.js
+++ client/src/components/Tiles/Manager/Manager.js
@@ -22,47 +22,49 @@ function Manager(props) {
   useEffect(() => {
     if (props.manager) {
       setOwner(props.manager);
-      loadAvatar(props.manager);
+      loadAvatar(owner);
     }
   }, [props.manager])
 
   function loadAvatar(user) {
     let avatarSettings = 'https://avataaars.io/?';
-
-    if (user.avatar.style) {
-      avatarSettings += 'avatarStyle=' + user.avatar.style
+    
+    if (user.avatar) {
+      if (user.avatar.style) {
+        avatarSettings += 'avatarStyle=' + user.avatar.style
+      }
+      if (user.avatar.top) {
+        avatarSettings += '&topType=' + user.avatar.top
+      }
+      if (user.avatar.accessories) {
+        avatarSettings += '&accessoriesType=' + user.avatar.accessories
+      }
+      if (user.avatar.hairColor) {
+        avatarSettings += '&hairColor=' + user.avatar.hairColor
+      }
+      if (user.avatar.facialHair) {
+        avatarSettings += '&facialHairType=' + user.avatar.facialHair
+      }
+      if (user.avatar.facialColor) {
+        avatarSettings += '&facialHairColor=' + user.avatar.facialColor
+      }
+      if (user.avatar.clothes) {
+        avatarSettings += '&clotheType=' + user.avatar.clothes
+      }
+      if (user.avatar.eyes) {
+        avatarSettings += '&eyeType=' + user.avatar.eyes
+      }
+      if (user.avatar.eyebrow) {
+        avatarSettings += '&eyebrowType=' + user.avatar.eyebrow
+      }
+      if (user.avatar.mouth) {
+        avatarSettings += '&mouthType=' + user.avatar.mouth
+      }
+      if (user.avatar.skin) {
+        avatarSettings += '&skinColor=' + user.avatar.skin
+      }
+      setAvatar(avatarSettings)
     }
-    if (user.avatar.top) {
-      avatarSettings += '&topType=' + user.avatar.top
-    }
-    if (user.avatar.accessories) {
-      avatarSettings += '&accessoriesType=' + user.avatar.accessories
-    }
-    if (user.avatar.hairColor) {
-      avatarSettings += '&hairColor=' + user.avatar.hairColor
-    }
-    if (user.avatar.facialHair) {
-      avatarSettings += '&facialHairType=' + user.avatar.facialHair
-    }
-    if (user.avatar.facialColor) {
-      avatarSettings += '&facialHairColor=' + user.avatar.facialColor
-    }
-    if (user.avatar.clothes) {
-      avatarSettings += '&clotheType=' + user.avatar.clothes
-    }
-    if (user.avatar.eyes) {
-      avatarSettings += '&eyeType=' + user.avatar.eyes
-    }
-    if (user.avatar.eyebrow) {
-      avatarSettings += '&eyebrowType=' + user.avatar.eyebrow
-    }
-    if (user.avatar.mouth) {
-      avatarSettings += '&mouthType=' + user.avatar.mouth
-    }
-    if (user.avatar.skin) {
-      avatarSettings += '&skinColor=' + user.avatar.skin
-    }
-    setAvatar(avatarSettings)
   }
 
   return (
